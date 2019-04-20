f1106e306ff1c12fdf31f5c3167e4b9c88a08cc5
Martin1887 <marcos.martin.pozo.delgado@gmail.com>
2019-04-20 12:02:09 +0200

    Initial commit

---
+++ action-shutdown.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*-: coding utf-8 -*-
+
+from hermes_python.hermes import Hermes
+import hermes_python
+import os
+
+if __name__ == "__main__":
+
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, res.decode("utf-8"))
+
+    os.system('sudo shutdown now -h')
