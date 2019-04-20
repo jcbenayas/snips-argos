9b8ef04e2a4546c9bc8fc587a0f0fb9aeb8458dc
Martin1887 <marcos.martin.pozo.delgado@gmail.com>
2019-04-20 12:43:29 +0200

    Fix issues

--- action-shutdown.py
+++ action-shutdown.py
@@ -5,9 +5,17 @@ from hermes_python.hermes import Hermes
 import hermes_python
 import os
 
-if __name__ == "__main__":
 
+def shutdown(hermes, intent_message):
     current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, res.decode("utf-8"))
+    hermes.publish_end_session(current_session_id, 'Shutting down')
 
     os.system('sudo shutdown now -h')
+
+
+if __name__ == "__main__":
+
+    with Hermes(MQTT_ADDR.encode("ascii")) as h:
+        h.subscribe_intent("Martin1887:ApagarSnips", shutdown) \
+            .subscribe_intent("Martin1887:ShutdownSnips", shutdown) \
+            .loop_forever()
